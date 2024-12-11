5db809a734de4e680c2647a830147b307d197e3c
PIYUJIN <yjyj020201@gmail.com>
2024-12-11 17:15:08 +0900

    [Fix] 권장 조치 수정 (API 35 ↑)

--- app/src/main/java/com/team/studing/MainActivity.kt
+++ app/src/main/java/com/team/studing/MainActivity.kt
@@ -27,7 +27,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        enableEdgeToEdge()
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         viewModel = ViewModelProvider(this)[LoginViewModel::class.java]
