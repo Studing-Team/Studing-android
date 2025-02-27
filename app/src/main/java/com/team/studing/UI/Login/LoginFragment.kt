3ab96a505a722eec7ac85d4c8410036f8c45f2d0
PIYUJIN <yjyj020201@gmail.com>
2025-02-27 21:46:53 +0900

    [Fix] #69  로그인 화면 디자인 변경

--- app/src/main/java/com/team/studing/UI/Login/LoginFragment.kt
+++ app/src/main/java/com/team/studing/UI/Login/LoginFragment.kt
@@ -3,19 +3,11 @@ package com.team.studing.UI.Login
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.os.Handler
-import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.content.res.ResourcesCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.skydoves.balloon.ArrowPositionRules
-import com.skydoves.balloon.Balloon
-import com.skydoves.balloon.BalloonAnimation
-import com.skydoves.balloon.BalloonSizeSpec
-import com.skydoves.balloon.showAlignTop
 import com.team.studing.LoginActivity
 import com.team.studing.R
 import com.team.studing.UI.SignUp.SignUpStep1Fragment
@@ -74,39 +66,6 @@ class LoginFragment : Fragment() {
     override fun onResume() {
         super.onResume()
         initView()
-        showToolTip()
-    }
-
-    fun showToolTip() {
-        val balloon = Balloon.Builder(loginActivity)
-//                .setWidth(BalloonSizeSpec.WRAP)
-            .setWidthRatio(0.6f) // sets width as 50% of the horizontal screen's size.
-            .setHeight(BalloonSizeSpec.WRAP)
-            .setText("스튜딩에 문의하기")
-            .setTextColorResource(R.color.black_30)
-            .setTextSize(12f)
-            .setTextTypeface(ResourcesCompat.getFont(loginActivity, R.font.inter_regular)!!)
-            .setArrowPositionRules(ArrowPositionRules.ALIGN_ANCHOR)
-            .setArrowSize(10)
-            .setArrowPosition(0.5f)
-            .setArrowColorResource(R.color.black_5)
-            .setTextGravity(Gravity.CENTER)
-            .setElevation(0)
-            .setPaddingHorizontal(15)
-            .setPaddingVertical(5)
-            .setMarginTop(10)
-            .setMarginBottom(10)
-            .setMarginHorizontal(35)
-            .setCornerRadius(16f)
-            .setBackgroundDrawableResource(R.drawable.background_tooltip_black5)
-            .setBalloonAnimation(BalloonAnimation.ELASTIC)
-            .build()
-
-        binding.buttonQna.showAlignTop(balloon)
-
-        Handler().postDelayed({
-            balloon.dismiss()
-        }, 3000)
     }
 
     fun initView() {
